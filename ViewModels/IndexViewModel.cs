431f37a2c3e4fef5b616c5e1e9f5ccb2bc243c93
ryanregal <85385482+ryanregal@users.noreply.github.com>
2023-09-13 17:53:09 +0800

    Add files via upload

---
+++ ViewModels/IndexViewModel.cs
@@ -0,0 +1,250 @@
+﻿using MyToDo.Common;
+using MyToDo.Common.Models;
+using MyToDo.Extensions;
+using MyToDo.Service;
+using MyToDo.Shared.Dtos;
+using Prism.Commands;
+using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Regions;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.ViewModels
+{
+    public class IndexViewModel : NavigationViewModel
+    {
+        private readonly IToDoService toDoService;
+        private readonly IMemoService memoService;
+        private readonly IDialogHostService dialog;
+        private readonly IRegionManager regionManager;
+
+        public IndexViewModel(IContainerProvider provider,
+            IDialogHostService dialog) : base(provider)
+        {
+            Title = $"校长大人！{AppSession.UserName} {DateTime.Now.GetDateTimeFormats('D')[1].ToString()}";
+            CreateTaskBars();
+            ExecuteCommand = new DelegateCommand<string>(Execute);
+            this.toDoService = provider.Resolve<IToDoService>();
+            this.memoService = provider.Resolve<IMemoService>();
+            this.regionManager = provider.Resolve<IRegionManager>();
+            this.dialog = dialog;
+            EditMemoCommand = new DelegateCommand<MemoDto>(AddMemo);
+            EditToDoCommand = new DelegateCommand<ToDoDto>(AddToDo);
+            ToDoCompltedCommand = new DelegateCommand<ToDoDto>(Complted);
+            NavigateCommand = new DelegateCommand<TaskBar>(Navigate);
+        }
+
+        private void Navigate(TaskBar obj)
+        {
+            if (string.IsNullOrWhiteSpace(obj.Target)) return;
+
+            NavigationParameters param = new NavigationParameters();
+
+            if (obj.Title == "已完成")
+            {
+                param.Add("Value", 2);
+            }
+            regionManager.Regions[PrismManager.MainViewRegionName].RequestNavigate(obj.Target, param);
+        }
+
+        private async void Complted(ToDoDto obj)
+        {
+            try
+            {
+                UpdateLoading(true);
+                var updateResult = await toDoService.UpdateAsync(obj);
+                if (updateResult.Status)
+                {
+                    var todo = summary.ToDoList.FirstOrDefault(t => t.Id.Equals(obj.Id));
+                    if (todo != null)
+                    {
+                        summary.ToDoList.Remove(todo);
+                        summary.CompletedCount += 1;
+                        summary.CompletedRatio = (summary.CompletedCount / (double)summary.Sum).ToString("0%");
+                        this.Refresh();
+                    }
+                    aggregator.SendMessage("已完成!");
+                }
+            }
+            finally
+            {
+                UpdateLoading(false);
+            }
+        }
+
+        public DelegateCommand<ToDoDto> ToDoCompltedCommand { get; private set; }
+        public DelegateCommand<ToDoDto> EditToDoCommand { get; private set; }
+        public DelegateCommand<MemoDto> EditMemoCommand { get; private set; }
+        public DelegateCommand<string> ExecuteCommand { get; private set; }
+
+        public DelegateCommand<TaskBar> NavigateCommand { get; private set; }
+
+        #region 属性
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; RaisePropertyChanged(); }
+        }
+
+        private ObservableCollection<TaskBar> taskBars;
+
+        public ObservableCollection<TaskBar> TaskBars
+        {
+            get { return taskBars; }
+            set { taskBars = value; RaisePropertyChanged(); }
+        }
+
+        private SummaryDto summary;
+
+        /// <summary>
+        /// 首页统计
+        /// </summary>
+        public SummaryDto Summary
+        {
+            get { return summary; }
+            set { summary = value; RaisePropertyChanged(); }
+        }
+
+        #endregion
+
+        private void Execute(string obj)
+        {
+            switch (obj)
+            {
+                case "新增咒灵": AddToDo(null); break;
+                case "新增教学": AddMemo(null); break;
+            }
+        }
+
+        /// <summary>
+        /// 添加咒灵
+        /// </summary>
+        async void AddToDo(ToDoDto model)
+        {
+            DialogParameters param = new DialogParameters();
+            if (model != null)
+                param.Add("Value", model);
+
+            var dialogResult = await dialog.ShowDialog("AddToDoView", param);
+            if (dialogResult.Result == ButtonResult.OK)
+            {
+                try
+                {
+                    UpdateLoading(true);
+                    var todo = dialogResult.Parameters.GetValue<ToDoDto>("Value");
+                    if (todo.Id > 0)
+                    {
+                        var updateResult = await toDoService.UpdateAsync(todo);
+                        if (updateResult.Status)
+                        {
+                            var todoModel = summary.ToDoList.FirstOrDefault(t => t.Id.Equals(todo.Id));
+                            if (todoModel != null)
+                            {
+                                todoModel.Title = todo.Title;
+                                todoModel.Content = todo.Content;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var addResult = await toDoService.AddAsync(todo);
+                        if (addResult.Status)
+                        {
+                            summary.Sum += 1;
+                            summary.ToDoList.Add(addResult.Result);
+                            summary.CompletedRatio = (summary.CompletedCount / (double)summary.Sum).ToString("0%");
+                            this.Refresh();
+                        }
+                    }
+                }
+                finally
+                {
+                    UpdateLoading(false);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 添加
+        /// </summary>
+        async void AddMemo(MemoDto model)
+        {
+            DialogParameters param = new DialogParameters();
+            if (model != null)
+                param.Add("Value", model);
+
+            var dialogResult = await dialog.ShowDialog("AddMemoView", param);
+            if (dialogResult.Result == ButtonResult.OK)
+            {
+                try
+                {
+                    UpdateLoading(true);
+                    var memo = dialogResult.Parameters.GetValue<MemoDto>("Value");
+
+                    if (memo.Id > 0)
+                    {
+                        var updateResult = await memoService.UpdateAsync(memo);
+                        if (updateResult.Status)
+                        {
+                            var todoModel = summary.MemoList.FirstOrDefault(t => t.Id.Equals(memo.Id));
+                            if (todoModel != null)
+                            {
+                                todoModel.Title = memo.Title;
+                                todoModel.Content = memo.Content;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var addResult = await memoService.AddAsync(memo);
+                        if (addResult.Status)
+                        {
+                            summary.MemoList.Add(addResult.Result);
+                        }
+                    }
+                }
+                finally
+                {
+                    UpdateLoading(false);
+                }
+            }
+        }
+
+        void CreateTaskBars()
+        {
+            TaskBars = new ObservableCollection<TaskBar>();
+            TaskBars.Add(new TaskBar() { Icon = "Adjust", Title = "咒灵汇总", Color = "#C38EC1", Target = "ToDoView" });
+            TaskBars.Add(new TaskBar() { Icon = "Nail", Title = "已拔除", Color = "#D46946", Target = "ToDoView" });
+            TaskBars.Add(new TaskBar() { Icon = "DogSide", Title = "拨除比例", Color = "#1999D8", Target = "" });
+            TaskBars.Add(new TaskBar() { Icon = "Sunglasses", Title = "高专教师", Color = "#95D4DE", Target = "MemoView" });
+        }
+
+        public override async void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            var summaryResult = await toDoService.SummaryAsync();
+            if (summaryResult.Status)
+            {
+                Summary = summaryResult.Result;
+                Refresh();
+            }
+            base.OnNavigatedTo(navigationContext);
+        }
+
+        void Refresh()
+        {
+            TaskBars[0].Content = summary.Sum.ToString();
+            TaskBars[1].Content = summary.CompletedCount.ToString();
+            TaskBars[2].Content = summary.CompletedRatio;
+            TaskBars[3].Content = summary.MemoeCount.ToString();
+        }
+    }
+}
---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,87 @@
+﻿using MyToDo.Common;
+using MyToDo.Extensions;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyToDo.Views
+{
+    /// <summary>
+    /// MainView.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainView : Window
+    {
+        private readonly IDialogHostService dialogHostService;
+
+        public MainView(IEventAggregator aggregator, IDialogHostService dialogHostService)
+        {
+            InitializeComponent();
+
+            //注册提示消息
+            aggregator.ResgiterMessage(arg =>
+            {
+                Snackbar.MessageQueue.Enqueue(arg);
+            });
+
+            //注册等待消息窗口
+            aggregator.Resgiter(arg =>
+            {
+                DialogHost.IsOpen = arg.IsOpen;
+
+                if (DialogHost.IsOpen)
+                    DialogHost.DialogContent = new ProgressView();
+            });
+
+            //三个按钮，最小最大关闭
+            btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; };
+            btnMax.Click += (s, e) =>
+            {
+                if (this.WindowState == WindowState.Maximized)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Maximized;
+            };
+            btnClose.Click += async (s, e) =>
+            {
+                var dialogResult = await dialogHostService.Question("温馨提示", "确认退出系统?");
+                if (dialogResult.Result != Prism.Services.Dialogs.ButtonResult.OK) return;
+                this.Close();
+            };
+
+            //最上方的那一条，可以用鼠标拖动
+            // += 是事件订阅操作符，s和e分别是事件发送者和事件参数
+            ColorZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    this.DragMove();
+            };
+
+            //通过双击可以让窗口最大最小化
+            ColorZone.MouseDoubleClick += (s, e) =>
+            {
+                if (this.WindowState == WindowState.Normal)
+                    this.WindowState = WindowState.Maximized;
+                else
+                    this.WindowState = WindowState.Normal;
+            };
+
+            //当动态菜单选定项改变的时候就将收缩
+            menuBar.SelectionChanged += (s, e) =>
+            {
+                drawerHost.IsLeftDrawerOpen = false;
+            };
+            this.dialogHostService = dialogHostService;
+        }
+    }
+}
